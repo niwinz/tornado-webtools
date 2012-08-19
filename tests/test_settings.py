17bdc7463948a299ee666d92b0c737c492f78ab3
Andrey Antukh <niwi@niwi.be>
2012-08-19 20:36:01 +0200

    Implemented modular class-based settings system.

---
+++ tests/test_settings.py
@@ -0,0 +1,32 @@
+import unittest
+
+class SampleTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        from webtools.settings import settings
+        cls.settings = settings
+
+
+    def test_access_undefined(self):
+        self.assertEqual(self.settings.FOO, None)
+
+    def test_access_some_default(self):
+        self.assertEqual(self.settings.TORNADO_SETTINGS, {})
+
+    def test_overwrite_1(self):
+        class mysettings(self.settings):
+            TORNADO_SETTINGS = {"foo": "bar"}
+
+        self.assertEqual(self.settings.TORNADO_SETTINGS, {"foo": "bar"})
+        self.settings.clear_to_default()
+        self.assertEqual(self.settings.TORNADO_SETTINGS, {})
+
+
+    def test_overwrite_2(self):
+        self.settings.configure({
+            "TORNADO_SETTINGS": {"foo": "bar"}
+        })
+
+        self.assertEqual(self.settings.TORNADO_SETTINGS, {"foo": "bar"})
+        self.settings.clear_to_default()
+        self.assertEqual(self.settings.TORNADO_SETTINGS, {})
---
+++ webtools/settings.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+class MetaSettings(type):
+    _settings = []
+
+    def __new__(cls, name, bases, attrs):
+        super_attrs, settings = {}, {}
+        for attr, val in attrs.items():
+            if attr.startswith("__"):
+                super_attrs[attr] = val
+            else:
+                settings[attr] = val
+
+        instance = super(MetaSettings, cls).__new__(cls, name, bases, super_attrs)
+        instance._settings.insert(0, settings)
+        return instance
+
+    def configure(cls, data):
+        assert isinstance(data, dict), "Invalid parameters"
+        cls._settings.insert(0, data)
+
+    def __getattr__(cls, key):
+        for settings_layer in cls._settings:
+            if key in settings_layer:
+                return settings_layer[key]
+
+        return None
+
+    def clear_to_default(cls):
+        original_settings = cls._settings[-1]
+        cls._settings = [original_settings]
+
+
+class settings(object, metaclass=MetaSettings):
+    SESSION_ENGINE = 'webtools.session.backend.database.DatabaseEngine'
+    SESSION_ENGINE_KWARGS = {}
+
+    AUTHENTICATION_BACKENDS = []
+
+    SQLALCHEMY_ENGINE_URL = None
+    SQLALCHEMY_ENGINE_KWARGS = {}
+
+    JINJA2_TEMPLATE_DIRS = []
+    JINJA2_SETTINGS = {"cache_size": 100}
+
+    TORNADO_TRANSFORMS = None
+    TORNADO_DEFAULT_HOST = ""
+    TORNADO_WSGI_MODE = False
+    TORNADO_SETTINGS = {}
+
+    SECRET_KEY = "sectet-key-change-me"
+
+
+__all__ = ['settings']
